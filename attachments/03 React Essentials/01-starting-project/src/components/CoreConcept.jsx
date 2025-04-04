0b65b84b1ed7657f40bc35f7bb715524833e45a5
Ada Vetrova <ninevital@yandex.ru>
2025-04-04 21:13:58 +0200

    refactor: restructure App component and add CoreConcept and TabButton components

---
+++ attachments/03 React Essentials/01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,11 @@
+export default function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt="Stylized atom" />
+      <h3>{title}</h3> 
+      <p>
+       {description}
+        </p>
+    </li>
+  );
+}
