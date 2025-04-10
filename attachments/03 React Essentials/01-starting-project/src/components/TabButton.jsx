c42f9ca3f504e98c290a2a8a361e2d194ebfa69c
Ada Vetrova <ninevital@yandex.ru>
2025-04-10 20:54:47 +0200

    added dynamic styling

--- attachments/03 React Essentials/01-starting-project/src/components/TabButton.jsx
+++ attachments/03 React Essentials/01-starting-project/src/components/TabButton.jsx
@@ -1,5 +1,5 @@
 export default function TabButton({children, onSelect, isSelected}) {
     return (
-        <li><button className={isSelected && "active"} onClick={onSelect}>{children}</button></li>
+        <li><button className={isSelected ? "active" : null} onClick={onSelect}>{children}</button></li>
     );
 }
